1b039ae139b2317a039a1c36607304d6e40267ca
Matthew Camilleri <79280069+Kryptic869@users.noreply.github.com>
2026-07-12 16:01:10 +0200

    Adding offers/new as a new route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+
 import { OffersPage } from './pages/offers/offersPage';
+import { AddOfferPage } from './pages/add-offer/addOfferPage';
 
 export const routes: Routes = [
     {
@@ -10,5 +12,13 @@ export const routes: Routes = [
     {
         path: 'offers',
         component: OffersPage
+    },
+    {
+        path: 'offers/new',
+        component: AddOfferPage
+    },
+    {
+        path: '**',
+        redirectTo: 'offers'
     }
 ];
