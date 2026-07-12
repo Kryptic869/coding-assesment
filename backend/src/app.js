5ba0c7aad48881983fa8858aa9c5e6771a85f073
Matthew Camilleri <79280069+Kryptic869@users.noreply.github.com>
2026-07-12 23:01:11 +0200

    Update app.js
    - App.js allowing cross-origin access to photos.
    - Express serves static files from the directory onto the backend, which are accessible via the URL.

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,16 +4,32 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import offerRoutes from './routes/offerRoutes.js';
 import businessRoutes from './routes/businessRoutes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(helmet());
+app.use(
+    helmet({
+        crossOriginResourcePolicy: {
+            policy: "cross-origin",
+        },
+    })
+);
 app.use(morgan('dev'));
 app.use(json());
 app.use("/api/offers", offerRoutes);
 app.use("/api/businesses", businessRoutes);
+
+app.use(
+    "/images",
+    express.static(path.join(__dirname, "../public/images"))
+);
 
 // Routes
 // Health check route
